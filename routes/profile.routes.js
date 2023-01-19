85ff3d1d3940d746ab2180ceca51cd8add20d1b7
Ana Belen Jover Ballester <anab.jover@gmail.com>
2023-01-19 09:13:29 +0100

    more implementations

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -1,13 +1,17 @@
 const router = require("express").Router();
+const isAuthenticated = require("../middlewares/isAuthenticated");
 const UserModel = require("../models/User.model");
 
+
 //GET "/api/admin/profile" => Datos de usuario
-router.get("/profile", async (req, res, next) => {
+router.get("/profile", isAuthenticated, async (req, res, next) => {
+    const {_id} = req.payload;    
 
     try {
 
-        const response = await req.session.username
+        const response = await UserModel.findById(_id);
         res.json(response)
+        console.log(response)
 
     } catch(error) {
         next(error)
